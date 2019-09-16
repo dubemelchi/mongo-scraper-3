cf6bc25263a4aa1bb2321f25713f9be41a963230
dubemelchi <dubemelchi@gmail.com>
2019-09-15 20:03:42 -0400

    clear js

---
+++ controllers/clear.js
@@ -0,0 +1,14 @@
+// variable to import models
+const db = require("../models");
+
+module.exports = {
+  clearDB: function(req, res) {
+    db.Headline.remove({})
+      .then(function() {
+        return db.Note.remove({});
+      })
+      .then(function() {
+        res.json({ ok: true });
+      });
+  }
+};
