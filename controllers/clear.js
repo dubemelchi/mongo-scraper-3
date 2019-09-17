96821a964394d5ee2bc04a0df273c78b67fb9e18
dubemelchi <dubemelchi@gmail.com>
2019-09-16 20:25:37 -0400

    clear js comments

--- controllers/clear.js
+++ controllers/clear.js
@@ -2,6 +2,7 @@
 const db = require("../models");
 
 module.exports = {
+  // whatever headline is queried will be removed
   clearDB: function(req, res) {
     db.Headline.remove({})
       .then(function() {
